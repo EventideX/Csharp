f2491325b1dd42f3349c16c6bcbb4f7839eb323b
EventideX <844058637@qq.com>
2017-10-25 19:31:45 +0800

    build

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] a = new int[2, 2] { { 1, 1 }, { 2, 2 } };
+            foreach (int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
